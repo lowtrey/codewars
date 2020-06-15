a82301b306ae4f30872c3b4cdbb7b90f9c00b584
Q. Price <priceqp@gmail.com>
2020-06-15 12:15:06 -0500

    complete number to string kata

---
+++ 8kyu/num_to_string.py
@@ -0,0 +1,21 @@
+# Instructions:
+# We need a function that can transform a number into a string.
+
+# What ways of achieving this do you know?
+
+# Examples:
+# number_to_string(123) /* returns '123' */
+# number_to_string(999) /* returns '999' */
+
+# My Solution
+def number_to_string(num):
+    return str(num)
+
+
+# Sample Tests
+test.expect(number_to_string(67) == '67')
+
+
+# Top Answer
+def number_to_string(num):
+    return str(num)
