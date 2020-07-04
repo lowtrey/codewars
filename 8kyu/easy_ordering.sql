f086360ab1bbb8986750406989d2eb1a04ca55f7
Q. Price <priceqp@gmail.com>
2020-07-03 20:32:56 -0500

    complete easy ordering kata

---
+++ 8kyu/easy_ordering.sql
@@ -0,0 +1,21 @@
+-- Instructions
+-- Your task is to sort the information in the provided table 'companies' 
+-- by number of employees (high to low). Returned table should be in the 
+-- same format as provided:
+
+-- companies table schema
+
+-- id
+-- ceo
+-- motto
+-- employees
+-- Solution should use pure SQL. Ruby is only used in test cases.
+
+
+-- My Solution
+SELECT * FROM companies
+  ORDER BY employees DESC
+
+
+-- Top Answer
+SELECT * FROM companies ORDER BY employees DESC
